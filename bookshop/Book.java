e543e1e3ff8189553723e18a418a6a80a18ecfb3
Lalithambigai S <127740404+Lalitha011004@users.noreply.github.com>
2026-06-01 13:41:01 +0530

    frist commit

--- bookshop/Book.java
+++ bookshop/Book.java
@@ -1,64 +1,76 @@
 package com.bookshop;
 
-public class Book 
+public class Book
 {
-	private int id;
-	private String name,author;
-	private float price;
-	
-	public Book() 
-	{
-	}
-	
-	public Book(int id, String name, String author, float price) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.author = author;
-		this.price = price;
-	}
-	public Book( String name, String author, float price) {
-		super();
-		this.name = name;
-		this.author = author;
-		this.price = price;
-	}
-	public int getId() {
-		return id;
-	}
+    private int bookId;
+    private String title, writer;
+    private float cost;
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public Book()
+    {
+    }
 
-	public String getName() {
-		return name;
-	}
+    public Book(int bookId, String title, String writer, float cost)
+    {
+        this.bookId = bookId;
+        this.title = title;
+        this.writer = writer;
+        this.cost = cost;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Book(String title, String writer, float cost)
+    {
+        this.title = title;
+        this.writer = writer;
+        this.cost = cost;
+    }
 
-	public String getAuthor() {
-		return author;
-	}
+    public int getBookId()
+    {
+        return bookId;
+    }
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
+    public void setBookId(int bookId)
+    {
+        this.bookId = bookId;
+    }
 
-	public float getPrice() {
-		return price;
-	}
+    public String getTitle()
+    {
+        return title;
+    }
 
-	public void setPrice(float price) {
-		this.price = price;
-	}
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
 
+    public String getWriter()
+    {
+        return writer;
+    }
 
-	@Override
-	public String toString() {
-		return "Book [id=" + id + ", name=" + name + ", author=" + author + ", price=" + price + "]";
-	}
-	
+    public void setWriter(String writer)
+    {
+        this.writer = writer;
+    }
+
+    public float getCost()
+    {
+        return cost;
+    }
+
+    public void setCost(float cost)
+    {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Book [bookId=" + bookId +
+               ", title=" + title +
+               ", writer=" + writer +
+               ", cost=" + cost + "]";
+    }
 }
